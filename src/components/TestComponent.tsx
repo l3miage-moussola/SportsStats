81ffe8b8d117c95388165e957e0172df41f2e324
l3miage-moussola <adam.moussolna@etu.univ-grenoble-alpes.fr>
2023-06-19 14:07:16 +0200

    commit

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+
+function TestComponent() {
+    return <Text>THIS COMPONENT WORKS!</Text>;
+  }
+export default TestComponent;
+
+
+
+
+
+
