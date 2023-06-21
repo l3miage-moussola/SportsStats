b4c2554112963fd12db7a56d7ebc475a7f76b903
l3miage-moussola <adam.moussolna@etu.univ-grenoble-alpes.fr>
2023-06-21 08:47:50 +0200

    feat : TestComponent loads leagues

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,13 +1,50 @@
-import React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import axios from 'axios';
 
-function TestComponent() {
-    return <Text>THIS COMPONENT WORKS!</Text>;
-  }
-export default TestComponent;
+function ExampleComponent() {
+    const [leagues, setLeagues] = useState<any[]>([]);
 
+    useEffect(() => {
+        const fetchLeagues = async () => {
+            try {
+                const response = await axios.get('https://v3.football.api-sports.io/leagues', {
+                    headers: {
+                        'x-apisports-key': 'YOUR_API_KEY' // Replace with your actual API key
+                    }
+                });
+                setLeagues(response.data?.response || []); // Add nullish coalescing operator and fallback to empty array
+            } catch (error) {
+                console.error('Error fetching leagues:', error);
+            }
+        };
 
+        fetchLeagues().then(r => leagues.push(r));
+    }, []);
 
+    if (leagues.length === 0) {
+        return <Text>Loading leagues...</Text>;
+    }
 
+    return (
+        <div>
+            <h2>Leagues</h2>
+            <ul>
+                {leagues.map((league: any) => (
+                    <li key={league.league.id}>
+                        <div>
+                            <img src={league.league.logo} alt={league.league.name} />
+                            <h3><Text>{league.league.name}</Text></h3>
+                        </div>
+                        <div>
+                            <p><Text>Country: {league.country.name}</Text></p>
+                            <p><Text>Seasons: {league.seasons.length}</Text></p>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
-
+export default ExampleComponent;
