91cee40dc7808822cda564982326666898adcb20
l3miage-moussola <adam.moussolna@etu.univ-grenoble-alpes.fr>
2023-06-25 21:50:22 +0200

    feat : les types du modèle des news

--- src/Models/FootballNewsModels.ts
+++ src/Models/FootballNewsModels.ts
@@ -3,7 +3,11 @@ export interface Competition {
   name: string;
 }
 export interface CompetitionNews {
+  NewsLink: string;
+  Image: string;
   title: string;
   publisherName: string;
   publisherLogo: string;
+  PublisherDate: string;
+
 }
